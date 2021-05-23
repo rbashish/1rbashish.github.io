af46403ca4669d29f03eb724d130dec669308f57
RB ASHISH <message.info1@gmail.com>
2021-05-24 01:07:30 +0530

    add cv site

---
+++ cv.rbashish.tech/sendemail.php
@@ -0,0 +1,71 @@
+<?php
+   
+
+    session_cache_limiter( 'nocache' );
+    header( 'Expires: ' . gmdate( 'r', 0 ) );
+    header( 'Content-type: application/json' );
+
+
+    $to         = 'message.info1@gmail.com';  // put your email here
+
+    $email_template = 'simple.html';
+
+    $subject    = strip_tags($_POST['subject']);
+    $email       = strip_tags($_POST['email']);
+    $phone      = strip_tags($_POST['phone']);
+    $name       = strip_tags($_POST['name']);
+    $message    = nl2br( htmlspecialchars($_POST['message'], ENT_QUOTES) );
+    $result     = array();
+
+
+    if(empty($name)){
+
+        $result = array( 'response' => 'error', 'empty'=>'name', 'message'=>'<strong>Error!</strong>&nbsp; Name is empty.' );
+        echo json_encode($result );
+        die;
+    } 
+
+    if(empty($email)){
+
+        $result = array( 'response' => 'error', 'empty'=>'email', 'message'=>'<strong>Error!</strong>&nbsp; Email is empty.' );
+        echo json_encode($result );
+        die;
+    } 
+
+    if(empty($message)){
+
+         $result = array( 'response' => 'error', 'empty'=>'message', 'message'=>'<strong>Error!</strong>&nbsp; Message body is empty.' );
+         echo json_encode($result );
+         die;
+    }
+    
+
+
+    $headers  = "From: " . $name . ' <' . $email . '>' . "\r\n";
+    $headers .= "Reply-To: ". $email . "\r\n";
+    $headers .= "MIME-Version: 1.0\r\n";
+    $headers .= "Content-Type: text/html; charset=UTF-8\r\n";
+
+
+    $templateTags =  array(
+        '{{subject}}' => $subject,
+        '{{email}}'=>$email,
+        '{{message}}'=>$message,
+        '{{name}}'=>$name,
+        '{{phone}}'=>$phone
+        );
+
+
+    $templateContents = file_get_contents( dirname(__FILE__) . '/email-templates/'.$email_template);
+
+    $contents =  strtr($templateContents, $templateTags);
+
+    if ( mail( $to, $subject, $contents, $headers ) ) {
+        $result = array( 'response' => 'success', 'message'=>'<strong>Oops!</strong>&nbsp; The message may not have been sent at this time. Instead you may write to <b>career@aashish.co.in</b> for faster communication.' );
+    } else {
+        $result = array( 'response' => 'error', 'message'=>'<strong>Oops!</strong>&nbsp; The message could not be sent at this time. Instead please send a mail to <b>career@aashish.co.in</b> for faster communication.'  );
+    }
+
+    echo json_encode( $result );
+
+    die;
