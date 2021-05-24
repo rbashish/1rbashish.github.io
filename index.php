96ace0af84f91f36b51c3a043f85ee24f0003020
RB ASHISH <message.info1@gmail.com>
2021-05-24 21:25:03 +0530

    update index
    added break line between footer and ads

--- index.php
+++ index.php
@@ -268,19 +268,20 @@ $(function(){
                 </section>-->
                 
                 <div class="text-center">
-		<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-<!-- Horizontal Display Ads -->
-<ins class="adsbygoogle"
-     style="display:block"
-     data-ad-client="ca-pub-3641110488583765"
-     data-ad-slot="5523567149"
-     data-ad-format="auto"
-     data-full-width-responsive="true"></ins>
-<script>
-     (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
-    </div>
-		
+			<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+				<!-- Horizontal Display Ads -->
+				<ins class="adsbygoogle"
+			    	 style="display:block"
+			     	data-ad-client="ca-pub-3641110488583765"
+			     	data-ad-slot="5523567149"
+			     	data-ad-format="auto"
+			    	 data-full-width-responsive="true"></ins>
+			<script>
+			     (adsbygoogle = window.adsbygoogle || []).push({});
+			</script>
+    		</div>
+		<br />
+		<br />
 		
       
 	
